53e756410392551b98cf94bf4b61248872f6d095
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-03-06 23:29:53 +0300

    first commit

---
+++ src/switch_statements/Switch_Statements.java
@@ -0,0 +1,30 @@
+
+package switch_statements;
+
+public class Switch_Statements {
+
+    public static void main(String[] args) {
+        int number=60;
+        switch (number)
+        {
+            case 10:
+                System.out.println("Your choice is 10");
+                break;
+            case 20:
+                System.out.println("Your choice is 20");
+                break;
+            case 30:
+                System.out.println("Your choice is 30");
+                break;
+            case 40:
+                System.out.println("Your choice is 40");
+                break;
+            case 50:
+                System.out.println("Your choice is 50");
+                break;
+            default:
+                System.out.println("Not in the range of 10,20,30,40,50");
+        }
+    }
+    
+}
